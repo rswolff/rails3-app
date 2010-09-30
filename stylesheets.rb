87b6e0870cea94918fc19d0df01f8fb82a7da5cf
Scott Wolff <scott.wolff@tuscan.ca>
2010-09-30 17:32:26 -0600

    adds some default app styles

--- stylesheets.rb
+++ stylesheets.rb
@@ -25,6 +25,33 @@ ul#nav li {
   
   file 'app.scss', <<-APP
 /* appy styles go here */
+$font-size: 18px;
+
+body {
+	background-color: #F6F6F6;
+	font-size: $font-size;
+}
+
+/* forms */
+
+label {
+	font-size: $font-size - 2;
+}
+
+input[type=text], input[type=password] {
+	font-size: $font-size;
+	padding: 4px;
+}
+
+input[type=submit] {
+	font-size: $font-size;
+}
+
+#flash_messages {
+	height: $font-size;
+	line-height: $font-size;	
+	padding: 15px;
+}
   APP
 end
 
