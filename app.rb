fb09f691694933555c24461f6273f57c90ca52b2
Scott Wolff <scott.wolff@tuscan.ca>
2012-01-24 08:06:35 -0700

    remove scaffold css, eliminates conflict with twitter bootstrap

--- app.rb
+++ app.rb
@@ -34,8 +34,6 @@ end
 
 #apply templates
 apply "#{root_dir}/gemfile.rb"
-apply "#{root_dir}/stylesheets.rb"
-apply "#{root_dir}/javascripts.rb"
 apply "#{root_dir}/layouts.rb"
 apply "#{root_dir}/generators.rb"
 apply "#{root_dir}/capistrano.rb"
@@ -45,6 +43,8 @@ apply "#{root_dir}/gitconfig.rb"
 apply "#{root_dir}/cleanup.rb"
 apply "#{root_dir}/setup.rb"
 apply "#{root_dir}/sorcery.rb"
+apply "#{root_dir}/stylesheets.rb"
+apply "#{root_dir}/javascripts.rb"
 
 git :init
 commit_state("initial commit")
--- stylesheets.rb
+++ stylesheets.rb
@@ -11,3 +11,7 @@ append_to_file 'app/assets/stylesheets/application.css' do
   'body {margin-top: 40px;}'
   '#flash {margin-top: 20px;}'
 end
+
+#remove scaffold styles, but leave scaffold.css in place.
+remove_file 'app/assets/stylesheets/scaffold.css'
+create_file 'app/assets/stylesheets/scaffold.css'
