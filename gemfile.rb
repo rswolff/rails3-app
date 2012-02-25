a809b1f95b66fb0793910ff5bbf6a65c759a1228
Scott Wolff <scott.wolff@tuscan.ca>
2012-02-25 13:03:17 -0700

    update gemfile for rails 3.2

--- gemfile.rb
+++ gemfile.rb
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+add_source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
 
@@ -18,7 +18,7 @@ gem 'whenever'
 
 # Gems used only for assets and not required
 # in production environments by default.
-group :assets do
+gem_group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 
