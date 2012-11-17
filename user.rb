1d445067897281105ef8717cd9e1bb84dae7bdd5
Scott Wolff <scott.wolff@tuscan.ca>
2012-11-16 23:48:00 -0700

    remove setup.rb and user.rb

--- user.rb
+++
@@ -1,3 +0,0 @@
-#create user scaffold
-say "Generate user scaffold: rails generate scaffold user ..."
-generate(:scaffold, "User first_name:string last_name:string email:string crypted_password:string salt:string")
