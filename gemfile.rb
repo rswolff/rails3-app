3684d032516acd80c7931da3bf2aeb281b942ff8
Scott Wolff <scott.wolff@tuscan.ca>
2013-01-02 17:11:55 -0700

    adds meta_request for RailsPanel support: https://github.com/dejan/rails_panel

--- gemfile.rb
+++ gemfile.rb
@@ -11,7 +11,11 @@ gem 'kaminari'
 gem 'prawn'
 gem 'state_machine'
 gem 'whenever'
-gem 'pry', group: 'development'
+
+gem_group :development do
+	gem 'pry'
+	gem 'meta_request', '0.2.1'	
+end
 
 gem_group :production do
   gem 'execjs'
